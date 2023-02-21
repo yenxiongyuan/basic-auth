78c7fdc37db53e7c96c146abca017f411b361084
Yen Xiong Yuan <yenyuan17@gmail.com>
2023-02-21 03:55:32 +0000

    updates

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ app.post('/signin', async (req, res) => {
     1. Find the user in the database by username
     2. Compare the plaintext password we now have against the encrypted password in the db
        - bcrypt does this by re-encrypting the plaintext password and comparing THAT
-    3. Either we're valid or we throw an error
+    3. Either we're valid or we throw an error.
   */
   try {
     const user = await Users.findOne({ where: { username: username } });
