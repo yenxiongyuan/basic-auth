d06903d502d11e19a652bc497145d5cb682d9aa6
Yen Xiong Yuan <yenyuan17@gmail.com>
2023-03-06 02:29:46 +0000

    finished lab 6

--- __tests__/auth.test.js
+++ __tests__/auth.test.js
@@ -2,20 +2,31 @@
 
 const { app } = require('../src/server');
 const supertest = require('supertest');
-const sequelizeDatabase = require('../src/auth/models');
-const { DESCRIBE } = require('sequelize/types/query-types');
+const { sequelizeDatabase } = require('../src/auth/models');
 const request = supertest(app);
 
+// Turn everything ON
 beforeAll(async() => {
   await sequelizeDatabase.sync();
 });
 
+// Turn everything OFF
 afterAll(async () => {
   await sequelizeDatabase.drop();
 });
 
-DESCRIBE('Auth router', () => {
+describe('Auth router', () => {
   it('creates a user', async () => {
-    let response = await request.post('/signup')
-  })
+    let response = await request.post('/signup').send({
+      username: 'Alpha',
+      password: 'password',
+    });
+    expect(response.status).toEqual(200);
+    expect(response.body.username).toEqual('Alpha');
+  });
+  it('allow user to signin', async () =>{
+    let response = await request.post('/signin').auth('Alpha', 'password');
+    expect(response.status).toEqual(200);
+    expect(response.body.username).toEqual('Alpha');
+  });
 });
